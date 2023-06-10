28a6c24f8072fc168b0228e25a84e4aa12cb98e3
Nathan <suprdory@google.com>
2023-06-10 01:06:09 +0100

    update selectin circle on drag input

--- d3.js
+++ d3.js
@@ -70,7 +70,7 @@ function updateData(){
         data.forEach(d => {
             let trackDat = JSON.parse(JSON.parse(d)['tracks']);
             let rprvDat = JSON.parse(JSON.parse(d)['rprv']);
-            plotTracks(trackDat, 2, 'coral', simtracks)
+            plotTracks(trackDat, 2, 'coral', simtracks,0.2)
             plotgraph(rprvDat, 2.0, 'coral',simlines);
         });
         simReady = true;
@@ -168,6 +168,16 @@ document.getElementById("radslider").onchange = function() {
     // console.log(this.value)
 };
 
+document.getElementById("radslider").oninput = function () {
+    selRad = this.value;
+    document.getElementById('textInput').value = selRad;
+    targetcirc.selectAll('*').remove()
+    plotCirc(coords,selRad)
+    // updateData();
+    // console.log(this.value)
+};
+
+
 d3.json("https://unpkg.com/world-atlas@1/world/110m.json").then(function (data) {
     land.selectAll(null)
         .data(topojson.feature(data, data.objects.land).features)
@@ -176,10 +186,10 @@ d3.json("https://unpkg.com/world-atlas@1/world/110m.json").then(function (data) 
         .attr("fill", "lightgreen")
         .attr("d", path);
 
-    d3.json("./tracks.geojson").then(function (data) {
-        plotTracks(data,1,'black',bgtracks,0.5)
+    // d3.json("./tracks.geojson").then(function (data) {
+    //     plotTracks(data,1,'black',bgtracks,0.5)
 
-    })
+    // })
 
 }
 
@@ -201,7 +211,7 @@ function circlePath(x0, y0, r) {
 var log = console.log
 // set up line graph
 // set the dimensions and margins of the graph
-var gmargin = { top: 10, right: 30, bottom: 35, left: 60 }
+var gmargin = { top: 10, right: 30, bottom: 38, left: 60 }
 var gwidth = 600 - gmargin.left - gmargin.right
 var gheight = 300 - gmargin.top - gmargin.bottom
 
@@ -286,91 +296,3 @@ let rvplot = plotgraphAxes();
 //             .attr("width", 200)
 //             .attr("height", 200)
 //     })
-
-
-
-
-// function LineChart(data, {
-//     x = ([x]) => x, // given d in data, returns the (temporal) x-value
-//     y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
-//     defined, // for gaps in data
-//     curve = d3.curveLinear, // method of interpolation between points
-//     marginTop = 20, // top margin, in pixels
-//     marginRight = 30, // right margin, in pixels
-//     marginBottom = 30, // bottom margin, in pixels
-//     marginLeft = 40, // left margin, in pixels
-//     width = 640, // outer width, in pixels
-//     height = 400, // outer height, in pixels
-//     xType = d3.scaleUtc, // the x-scale type
-//     xDomain, // [xmin, xmax]
-//     xRange = [marginLeft, width - marginRight], // [left, right]
-//     yType = d3.scaleLinear, // the y-scale type
-//     yDomain, // [ymin, ymax]
-//     yRange = [height - marginBottom, marginTop], // [bottom, top]
-//     yFormat, // a format specifier string for the y-axis
-//     yLabel, // a label for the y-axis
-//     color = "currentColor", // stroke color of line
-//     strokeLinecap = "round", // stroke line cap of the line
-//     strokeLinejoin = "round", // stroke line join of the line
-//     strokeWidth = 1.5, // stroke width of line, in pixels
-//     strokeOpacity = 1, // stroke opacity of line
-// } = {}) {
-//     // Compute values.
-//     const X = d3.map(data, x);
-//     const Y = d3.map(data, y);
-//     const I = d3.range(X.length);
-//     if (defined === undefined) defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
-//     const D = d3.map(data, defined);
-
-//     // Compute default domains.
-//     if (xDomain === undefined) xDomain = d3.extent(X);
-//     if (yDomain === undefined) yDomain = [0, d3.max(Y)];
-
-//     // Construct scales and axes.
-//     const xScale = xType(xDomain, xRange);
-//     const yScale = yType(yDomain, yRange);
-//     const xAxis = d3.axisBottom(xScale).ticks(width / 80).tickSizeOuter(0);
-//     const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
-
-//     // Construct a line generator.
-//     const line = d3.line()
-//         .defined(i => D[i])
-//         .curve(curve)
-//         .x(i => xScale(X[i]))
-//         .y(i => yScale(Y[i]));
-
-//     const svg = d3.create("svg")
-//         .attr("width", width)
-//         .attr("height", height)
-//         .attr("viewBox", [0, 0, width, height])
-//         .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-//     svg.append("g")
-//         .attr("transform", `translate(0,${height - marginBottom})`)
-//         .call(xAxis);
-
-//     svg.append("g")
-//         .attr("transform", `translate(${marginLeft},0)`)
-//         .call(yAxis)
-//         .call(g => g.select(".domain").remove())
-//         .call(g => g.selectAll(".tick line").clone()
-//             .attr("x2", width - marginLeft - marginRight)
-//             .attr("stroke-opacity", 0.1))
-//         .call(g => g.append("text")
-//             .attr("x", -marginLeft)
-//             .attr("y", 10)
-//             .attr("fill", "currentColor")
-//             .attr("text-anchor", "start")
-//             .text(yLabel));
-
-//     svg.append("path")
-//         .attr("fill", "none")
-//         .attr("stroke", color)
-//         .attr("stroke-width", strokeWidth)
-//         .attr("stroke-linecap", strokeLinecap)
-//         .attr("stroke-linejoin", strokeLinejoin)
-//         .attr("stroke-opacity", strokeOpacity)
-//         .attr("d", line(I));
-
-//     return svg.node();
-// }
